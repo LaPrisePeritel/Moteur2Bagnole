4edcc842742912d75ef081e43aaff3adc92b6bdf
Lynix <lynix680@gmail.com>
2022-09-20 12:04:10 +0200

    Ajout petite animation

--- src/main.cpp
+++ src/main.cpp
@@ -22,11 +22,29 @@ int main(int argc, char** argv)
 
     Uint64 lastUpdate = SDL_GetPerformanceCounter();
 
+    int frameIndex = 0;
+    int frameCount = 5;
+    float timer = 0.0f;
+
     bool isOpen = true;
     while (isOpen)
     {
         Uint64 now = SDL_GetPerformanceCounter();
         float deltaTime = (float) (now - lastUpdate) / SDL_GetPerformanceFrequency();
+        lastUpdate = now;
+
+        timer += deltaTime;
+        if (timer > 0.1f)
+        {
+            timer -= 0.1f;
+            frameIndex++;
+            if (frameIndex >= frameCount)
+                frameIndex = 0;  
+
+            sprite.SetRect({ frameIndex * 32, 0, 32, 32 });
+
+            std::cout << frameIndex << std::endl;
+        }
 
         SDL_Event event;
         while (SDLpp::PollEvent(&event))
