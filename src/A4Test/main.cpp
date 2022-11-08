4788a2bc6baab925753a58db625019a78397eb37
Lynix <lynix680@gmail.com>
2022-11-08 09:16:26 +0100

    Début exo ambulance

--- src/A4Test/main.cpp
+++ src/A4Test/main.cpp
@@ -1,6 +1,14 @@
 #include <AL/al.h>
 #include <AL/alc.h>
 #define DR_WAV_IMPLEMENTATION
+#include <A4Engine/CameraComponent.hpp>
+#include <A4Engine/GraphicsComponent.hpp>
+#include <A4Engine/ResourceManager.hpp>
+#include <A4Engine/Sprite.hpp>
+#include <A4Engine/Transform.hpp>
+#include <A4Engine/SDLpp.hpp>
+#include <A4Engine/SDLppRenderer.hpp>
+#include <A4Engine/SDLppWindow.hpp>
 #include <dr_wav.h>
 #include <iostream>
 #include <string>
@@ -26,12 +34,9 @@ int main()
 	ALCcontext* context = alcCreateContext(device, nullptr);
 	alcMakeContextCurrent(context);
 
-	ALuint buffer;
-	alGenBuffers(1, &buffer);
-
 	// On va faire des trucs !
 	drwav wav;
-	if (!drwav_init_file(&wav, "assets/MaenderAlkoor.wav", nullptr))
+	if (!drwav_init_file(&wav, "assets/siren.wav", nullptr))
 	{
 		std::cout << "failed to load file" << std::endl;
 		return 0;
@@ -40,7 +45,14 @@ int main()
 	std::vector<std::int16_t> samples(wav.totalPCMFrameCount * wav.channels);
 	drwav_read_pcm_frames_s16(&wav, wav.totalPCMFrameCount, samples.data());
 
-	alBufferData(buffer, (wav.channels == 2) ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16, samples.data(), samples.size(), wav.sampleRate);
+	ALuint buffer;
+	alGenBuffers(1, &buffer);
+
+	alBufferData(buffer, 
+		(wav.channels == 2) ? AL_FORMAT_STEREO16 : AL_FORMAT_MONO16, 
+		samples.data(), 
+		samples.size() * sizeof(std::int16_t),
+		wav.sampleRate);
 
 	drwav_uninit(&wav);
 
@@ -49,11 +61,53 @@ int main()
 
 	alSourcei(source, AL_BUFFER, buffer);
 	alSourcei(source, AL_LOOPING, AL_TRUE);
-	alSourcePlay(source);
 
-	std::getchar();
+	alSource3f(source, AL_POSITION, 50.f, 0.f, 0.f);
+
+	alListener3f(AL_POSITION, 45.f, 0.f, 0.f);
+
+	//alSourcePlay(source);
+
+	SDLpp sdl;
+	SDLppWindow window("Test audio", 1280, 720);
+	SDLppRenderer renderer(window, "", SDL_RENDERER_PRESENTVSYNC);
+
+	ResourceManager resourceManager(renderer);
+
+	Sprite ambulance(resourceManager.GetTexture("assets/ambulance.png"));
+	ambulance.Resize(ambulance.GetWidth() * 0.1f, ambulance.GetHeight() * 0.1f);
+	ambulance.SetOrigin(Vector2f(0.5f, 0.5f));
+
+	Transform cameraTransform;
+	Transform ambulanceTransform;
+	ambulanceTransform.SetPosition(Vector2f(640, 360));
+
+	Uint64 lastUpdate = SDL_GetPerformanceCounter();
+
+	bool isOpen = true;
+	while (isOpen)
+	{
+		Uint64 now = SDL_GetPerformanceCounter();
+		float deltaTime = (float)(now - lastUpdate) / SDL_GetPerformanceFrequency();
+		lastUpdate = now;
+
+		SDL_Event event;
+		while (sdl.PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				isOpen = false;
+		}
+		
+		renderer.SetDrawColor(0, 127, 255, 255);
+		renderer.Clear();
+
+		ambulance.Draw(renderer, cameraTransform, ambulanceTransform);
+
+		renderer.Present();
+	}
 
 	// Libération
+	alDeleteSources(1, &source);
 	alDeleteBuffers(1, &buffer);
 
 	alcMakeContextCurrent(nullptr);
